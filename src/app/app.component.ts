e6d51f6fe883f583c34970921caed1a68c65c533
misostack <misostack.com@gmail.com>
2020-02-27 00:22:27 +0700

    Add loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 const SLIDES = [
 	{
-		'title': 'Component',
-		'description': `
+		title: 'Component',
+		description: `
 			Basically is a set of function ( implemented as a class with decoration ) which instructs NgModules render the associated HTML
 		`,
 	},
 	{
-		'title': 'Template',
-		'description': `
-		HTML files, also includes Angular Directives and Binding Markup
-		`,
-		'bullets': [
+		title: 'Template',
+		description: `HTML files, also includes Angular Directives and Binding Markup`,
+		bullets: [
 			`Interpolation: Using expression '{{value}}' to interpolate value properties in component into HTML`
 		]
 	}
@@ -27,9 +25,11 @@ export class AppComponent {
   title = 'Angular';
   description = '<p><strong>HTML String</strong></p>'
   aLink = '<a href="http://localhost:4200">Angular App Local</a>'
-
+  slides: Array<{}>
+  @ViewChild('bindingInput') bindingInput: ElementRef
 
   constructor(){
   	console.log('constructor')
+  	this.slides = SLIDES
   }
 }
